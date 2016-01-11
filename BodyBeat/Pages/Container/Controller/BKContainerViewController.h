ec298bd443a35cd87b3cdd32db707260b71fbaff
Behran Kankul <behrank@gmail.com>
2016-01-11 14:30:40 +0200

    new controllers added

---
+++ BodyBeat/Pages/Container/Controller/BKContainerViewController.h
@@ -0,0 +1,13 @@
+//
+//  BKContainerViewController.h
+//  BodyBeat
+//
+//  Created by Can Behran Kankul on 11/01/16.
+//  Copyright © 2016 Can Behran Kankul. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BKContainerViewController : UIViewController
+
+@end
---
+++ BodyBeat/Pages/Email Login/Controller/BKEmailLoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  BKEmailLoginViewController.h
+//  BodyBeat
+//
+//  Created by Can Behran Kankul on 11/01/16.
+//  Copyright © 2016 Can Behran Kankul. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BKEmailLoginViewController : UIViewController
+
+@end
